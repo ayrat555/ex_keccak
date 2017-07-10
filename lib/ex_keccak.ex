e6e47240070b2bc363dd7c932e54a8169834313a
Ayrat Badykov <ayratin555@gmail.com>
2017-07-10 09:40:06 +0300

    intial comiit

---
+++ lib/ex_keccak.ex
@@ -0,0 +1,18 @@
+defmodule ExKeccak do
+  @moduledoc """
+  Documentation for ExKeccak.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExKeccak.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_keccak_test.exs
@@ -0,0 +1,8 @@
+defmodule ExKeccakTest do
+  use ExUnit.Case
+  doctest ExKeccak
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
